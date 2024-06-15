2fbbdb47cf721d5a7e6b3ecebff1c37e91980254
Guilherme Weber <gh63388@gmail.com>
2024-06-15 14:08:01 -0300

    Atualizando as configuracoes de variavies de ambiente e add o .env

--- API/src/config/config.go
+++ API/src/config/config.go
@@ -1,5 +1,13 @@
 package config
 
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+	"strconv"
+)
+
 var (
 	// StringConexaoBanco é a string q conecta com o mysql
 	StringConexaoBanco = ""
@@ -9,5 +17,21 @@ var (
 
 // Carregar vai inicializar as variaveis de ambientes
 func Carregar() {
+	var erro error
+
+	if erro = godotenv.Load(); erro != nil {
+		log.Fatal(erro)
+	}
+
+	Porta, erro = strconv.Atoi(os.Getenv("API_PORT"))
+	if erro != nil {
+		Porta = 9000
+	}
+
+	StringConexaoBanco = fmt.Sprintf("%s:%s@/%s?charset=utf8&parseTime=True&loc=Local",
+		os.Getenv("DB_USUARIO"),
+		os.Getenv("DB_SENHA"),
+		os.Getenv("DB_NOME"),
+	)
 
 }
