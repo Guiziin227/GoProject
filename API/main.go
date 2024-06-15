4d2344220200f02aea9f10cb531d4d4996763268
Guilherme Weber <gh63388@gmail.com>
2024-06-15 14:11:41 -0300

    Adicionando as configuracoes de V.A no main

--- API/main.go
+++ API/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"api/src/config"
 	"api/src/router"
 	"fmt"
 	"log"
@@ -9,10 +10,13 @@ import (
 
 func main() {
 
+	config.Carregar()
+
+	fmt.Println(config.StringConexaoBanco)
 	fmt.Println("Rodando a api")
 
 	r := router.Gerar()
 
-	log.Fatal(http.ListenAndServe(":5000", r))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Porta), r))
 
 }
