80fa96f455552fe845d8efbaea5aab8465638f8e
Guilherme Weber <gh63388@gmail.com>
2024-06-15 13:55:18 -0300

    Adicionando a config.go

---
+++ API/src/config/config.go
@@ -0,0 +1,13 @@
+package config
+
+var (
+	// StringConexaoBanco é a string q conecta com o mysql
+	StringConexaoBanco = ""
+	// Porta onde a api vai estar rodando
+	Porta = 0
+)
+
+// Carregar vai inicializar as variaveis de ambientes
+func Carregar() {
+
+}
