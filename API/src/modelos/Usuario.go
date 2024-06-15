a82a5649011f005590c0c2f5fdbad885de21f96d
Guilherme Weber <gh63388@gmail.com>
2024-06-15 13:24:26 -0300

    Criando struct de usuarios

--- API/src/modelos/Usuario.go
+++ API/src/modelos/Usuario.go
@@ -1,4 +1,12 @@
 package modelos
 
-type usuario struct {
+import "time"
+
+type Usuario struct {
+	ID       uint64    `json:"id,omitempty"` //omitempty se vazio nao passa para o json.
+	Name     string    `json:"name,omitempty"`
+	Nick     string    `json:"nick,omitempty"`
+	Email    string    `json:"email,omitempty"`
+	Senha    string    `json:"senha,omitempty"`
+	CriadoEm time.Time `json:"criado_em,omitempty"`
 }
